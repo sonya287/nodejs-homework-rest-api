3961ce0cb41435e322cfd721aaef3925dc7eb231
sonya287 <starodyb1987@gmail.com>
2021-10-26 14:19:34 +0300

    HW 5

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,17 @@
 const app = require('../app')
 const db = require('../model/db')
+const createFolderIsExist = require('../helpers/create-dir')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
 db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running. Use our API on port: ${PORT}`)
+  app.listen(PORT, async () => {
+    const UPLOAD_DIR = process.env.UPLOAD_DIR
+    const SAVE_DIR = process.env.SAVE_DIR
+    await createFolderIsExist(UPLOAD_DIR)
+    await createFolderIsExist(SAVE_DIR)
+    console.log(`Server is not runnig. Use our API on port: ${PORT}`)
   })
 }).catch((error) => {
   console.log(`Server is not running. Error message: ${error.message}`)
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken')
+const fs = require('fs').promises
+const path = require('path')
+const Jimp = require('jimp')
 const Users = require('../model/users')
 const { HttpCode } = require('../helpers/constants')
+const createFolderIsExist = require('../helpers/create-dir')
 require('dotenv').config()
 const SECRET_KEY = process.env.JWT_SECRET
 
@@ -24,6 +28,7 @@ const register = async (req, res, next) => {
             code: HttpCode.CREATED,
             data: {
                 email: newUser.email,
+                avatarURL: newUser.avatarURL,
                 subscription: newUser.subscription
             }
         })
@@ -73,6 +78,42 @@ const logout = async (req, res, next) => {
     return res.status(HttpCode.NO_CONTENT).json()
 }
 
+const addAvatar = async (req, res, next) => {
+    try {
+        const id = await req.user.id
+        const SAVE_DIR = process.env.SAVE_DIR
+        const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
+        const pathFile = req.file.path
+        const newPathFile = path.join(SAVE_DIR, AVATARS_OF_USERS)
+        const newAvatarName = `${Date.now()}-${req.file.originalname}`
+        const img = await Jimp.read(pathFile)
+        await img
+            .autocrop()
+            .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
+            .writeAsync(pathFile)
+
+        await createFolderIsExist(newPathFile)
+        await createFolderIsExist(path.join(newPathFile, id))
+        await fs.rename(pathFile, path.join(newPathFile, id, newAvatarName))
+        const avatarUrl = path.normalize(path.join(AVATARS_OF_USERS, id, newAvatarName))
+        try {
+            await fs.unlink(path.join(process.cwd(), SAVE_DIR, req.user.avatarURL))
+        } catch (e) {
+            console.log(e.message)
+        }
+        await Users.updateAvatar(id, avatarURL)
+        return res.json({
+            status: 'succes',
+            code: HttpCode.SUCCESS,
+            data: {
+                avatarURL
+            }
+        })
+    } catch (e) {
+        next(e)
+    }
+}
+
 const getCurrent = async (req, res, next) => {
     try {
         return res.status(HttpCode.SUCCESS).json({
@@ -110,4 +151,4 @@ const update = async (req, res, next) => {
     }
 }
 
-module.exports = { register, login, logout, getCurrent, update }
+module.exports = { register, login, logout, addAvatar, getCurrent, update }
